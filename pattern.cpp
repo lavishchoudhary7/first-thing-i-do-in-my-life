9d38cfda8b62bf928bfeeea3f7b9bffcf32262de
Lavish Choudhary <lavishchoudhary698@gmail.com>
2026-09-05 06:57:25 -0700

    Updated code

--- pattern.cpp
+++ pattern.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 int main()
 {
-    int n;
-    cout<<"Enter the number of rows: "
+    int i,j;
+
+for (i = 1; i <= 5; i++)
+{
+    for (j = 1; j <= 5 - i; j++)
+        cout << " ";
+    for (j = 1; j <= 2 * i - 1; j++)
+        cout << "*";
+    cout << endl;
+}
+return 0;
+
 }
