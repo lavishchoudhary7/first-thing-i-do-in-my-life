efb2f4bcfd19b98b140cfc287faf2d954d88fd53
Lavish Choudhary <lavishchoudhary698@gmail.com>
2026-09-06 11:12:01 -0700

    Added prime number program

---
+++ pt1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n ,i;
+
+    if(n<2)
+    {
+cout <<"not prime";
+
+    }
+    else{
+        for(int i=2; i<n;i=i+1)
+        {
+            if (n%i==0)
+            {cout<<"not prime";}
+        }
+        cout<<"prime";
+    } 
+}
